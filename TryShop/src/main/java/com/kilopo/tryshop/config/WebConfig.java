ce010aebbe969877dc5adb4ec69225b11991a3b5
kilopo <marian.brynetskyy@outlook.com>
2016-12-31 11:13:12 +0200

    Shop-2 Skeleton

---
+++ TryShop/src/main/java/com/kilopo/tryshop/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.kilopo.tryshop.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.kilopo.tryshop")
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+}
